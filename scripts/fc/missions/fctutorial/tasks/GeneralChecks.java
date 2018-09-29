87bd5e623d10b86e59030d2eb12da4cd22b2019b
Final Calibur <fmorris2@oswego.edu>
2018-09-29 11:44:52 -0400

    Fixed issue with clicking report button

--- scripts/fc/missions/fctutorial/tasks/GeneralChecks.java
+++ scripts/fc/missions/fctutorial/tasks/GeneralChecks.java
@@ -10,10 +10,8 @@ import scripts.fc.framework.task.Task;
 public class GeneralChecks extends Task
 {
 	private static final long serialVersionUID = -8065901942838995357L;
-	private final int CLICK_CONTINUE_MASTER = 162;
-	private final int CLICK_CONTINUE_CHILD = 33;
 	private final int LOGS_MESSAGE = 193;
-	private final int LOGS_CHILD = 2;
+	private final int LOGS_CHILD = 3;
 	
 	private RSInterface inter;
 	
@@ -30,11 +28,7 @@ public class GeneralChecks extends Task
 	@Override
 	public boolean shouldExecute()
 	{
-		inter = Interfaces.get(CLICK_CONTINUE_MASTER, CLICK_CONTINUE_CHILD);
-		
-		if(inter == null || inter.isHidden())
-			inter = Interfaces.get(LOGS_MESSAGE, LOGS_CHILD);
-		
+		inter = Interfaces.get(LOGS_MESSAGE, LOGS_CHILD);
 		return inter != null && !inter.isHidden();
 	}
 
